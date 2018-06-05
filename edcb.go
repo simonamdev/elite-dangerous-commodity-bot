61c4743c8fdc717dc2c06308e35378acecd6d50c
Simon AM <simon@agius-muscat.net>
2018-06-05 12:53:45 +0200

    Added basic Go function

--- edcb.go
+++ edcb.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
     fmt.Println("Hello, EDCB")
+    getClosestCommoditySystem("Narcotics", "Brestla")
+}
+
+func getClosestCommoditySystem(commodity string, system string) {
+    s := fmt.Sprintf("Finding  closest system to %s which sells commodity: %s", system, commodity)
+    fmt.Println(s);
 }
