196fa31b49f958c3e568983b7fa6129866b0c4fd
Simon AM <simon@agius-muscat.net>
2018-06-16 00:23:26 +0200

    Removed old JS file

--- src/edcb.js
+++
@@ -1,9 +0,0 @@
-export default class EliteDangerousCommdityBot {
-    constructor(debug) {
-        this.debug = debug;
-    }
-
-    init() {
-        console.log(`Initialising in Debug mode: ${this.debug}`);
-    }
-}
